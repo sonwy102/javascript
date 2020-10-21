c95bd914b6c7c2904fd790fcbc766835fcbf440b
Wooyang Son <sonwy102@gmail.com>
2020-10-21 16:56:01 +0000

    Added JS translation

--- melonCounting.js
+++ melonCounting.js
@@ -1,6 +1,7 @@
 // Javascript translation of melon_counting.py
+"use-strict";
 
-melons_to_add = [
+const melonsToAdd = [
   "Ogen", "Horned Melon", "Watermelon", "Casaba", "Sharlyn", "Xigua",
   "Ogen", "Christmas", "Christmas", "Christmas", "Christmas", "Watermelon",
   "Sharlyn", "Xigua", "Cantaloupe", "Christmas", "Watermelon", "Christmas",
@@ -11,3 +12,18 @@ melons_to_add = [
   "Horned Melon", "Xigua", "Xigua", "Watermelon", "Cantaloupe", "Casaba",
   "Cantaloupe", "Casaba", "Watermelon", "Santa Claus", "Casaba",
 ]; 
+
+function countMelons(melonList) {
+
+    const melonCounts = {};
+
+    for (const melon of melonList) {
+        if (!melonCounts[melon]) {
+            melonCounts[melon] = 1;
+        } else {
+            melonCounts[melon] += 1;
+        }
+    }
+
+    return melonCounts;
+}
